a66761b720c198e6f342eb0f4b2eee49fca6d97b
Varun Swaminathan <85042815+flerovious@users.noreply.github.com>
2023-04-21 23:54:11 -0700

    Add upload files endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,25 @@
 from fastapi import FastAPI
+from typing import Annotated
+
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import HTMLResponse
+
 
 app = FastAPI()
 
 
+@app.post("/uploadfiles/")
+async def create_upload_files(files: list[UploadFile]):
+    return {"filenames": [file.filename for file in files]}
+
+
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def main():
+    content = """
+<form action="/uploadfiles/" enctype="multipart/form-data" method="post">
+<input name="files" type="file" webkitdirectory multiple>
+<input type="submit">
+</form>
+</body>
+    """
+    return HTMLResponse(content=content)
