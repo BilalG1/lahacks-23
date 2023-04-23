a5d696e1718dfceff05523c7b4739ed74f48cc9f
BilalG1 <bg2002@gmail.com>
2023-04-23 03:41:40 -0700

    port

--- app.py
+++ app.py
@@ -275,4 +275,4 @@ def select_dockerfile(tech_stack):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=3000)
