661d76fc7c924743bd30685f37cfe4a032812ad4
BilalG1 <bg2002@gmail.com>
2023-04-23 02:32:16 -0700

    typo

--- front/src/constants/config.ts
+++ front/src/constants/config.ts
@@ -1,5 +1,5 @@
 export const config = {
   isDemo: false,
-  apiUrl: import.meta.env.PROD ? 'https://fullstack-api.bazzled.com/' : 'http://localhost:8000',
+  apiUrl: import.meta.env.PROD ? 'https://fullstack-api.bazzled.com' : 'http://localhost:8000',
   
 }
